9255ff0c4c88f3a66b407187fa5ad5335f087275
Soji <ahsojitra@gmail.com>
2018-12-15 17:51:38 -0500

    stuff

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./tokyoBoy.js",
+  entry: "./pizzaface.js",
   output: {
     path: path.join(__dirname, 'assets', 'lib'),
     filename: "bundle.js"
